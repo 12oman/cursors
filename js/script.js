4bb407d22b4004a301bad0110979659812c27214
Roman Mitch <romanmitch@gmail.com>
2023-05-25 22:47:19 +1200

    add basic cursors

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// window.onload = () => {
+//   let divs = document.querySelectorAll("div");
+//   divs.forEach((div) => {
+//     // avoid overwriting the innerHTML of any nested divs
+//     if (div.children.length > 0) return;
+//     div.innerHTML = div.className;
+//   });
+// };
